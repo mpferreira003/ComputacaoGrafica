845621f68278081ea8b5e00c34ab4e713a376cea
Antonio Luiz Carvalho Turano Filho <turano@usp.br>
2025-03-27 17:05:26 -0300

    vsf bom dia

--- src/trabalho1/area.py
+++ src/trabalho1/area.py
@@ -1,12 +1,47 @@
 import posmath
 import numpy as np
+from OpenGL.GL import glClear, glClearColor, GL_COLOR_BUFFER_BIT
 
 class Area():
+
+    _x = 0
+    _y = 0
+    _angle = 0
+
+    tx = 0
+    ty = 0
+    tangle = 0
+
     def __init__(self,vertices):
-        self.x,self.y = 0 ## considera que o objeto foi instanciado no 0,0
+        self.x,self.y = 0, 0 ## considera que o objeto foi instanciado no 0,0
         self.mat_transform = np.eye(4) ## considera que o objeto está no 0,0, com 0 graus e escala 1
+    
+    def modify(self, x, y, angle):
+        self.tx = x
+        self.ty = y
+        self.tangle = angle
+
+    def reset(self):
+        self.tx = 0
+        self.ty = 0
+        self.tangle = 0
+
     def draw(self):
-        pass
+        self._x += self.tx
+        self._y += self.ty
+        self._angle += self.tangle
+
+        matriz_transformacao = np.array([    1.0, 0.0, 0.0, 0.0, 
+                                    0.0, 1.0, 0.0, 0.0, 
+                                    0.0, 0.0, 1.0, 0.0, 
+                                    0.0, 0.0, 0.0, 1.0], np.float32)
+        if(self.tangle):
+            matriz_transformacao = self.rotateOn()
+
+        self.reset()
+        
+        return matriz_transformacao
+
     def transform(self):
         """
         Aplica a transformação em todos os vértices
@@ -17,15 +52,15 @@ class Area():
     
     def move(self,x,y,relative=True):
         pass
-    def rotateOn(pivot,relative=True):
+    def rotateOn(self):
         # 1. Translação para a origem
-        pos = get_transl_matrix(-tx, -ty)
+        pos = posmath.get_transl_matrix(-self._x, -self._y)
         
         # 2. Rotação em torno da origem
-        rotation = get_rot_matrix(angle)
+        rotation = posmath.get_rot_matrix(self._angle)
         
         # 3. Translação de volta para a posição original
-        pos_anti = get_transl_matrix(tx, tty)
+        pos_anti = posmath.get_transl_matrix(self._x, self._y)
         
         # Combina as matrizes: pos_anti @ rotation @ pos
         # return pos_anti @ rotation @ pos
--- src/trabalho1/main.py
+++ src/trabalho1/main.py
@@ -1,13 +1,10 @@
 import glfw
 from OpenGL.GL import *
-import OpenGL.GL.shaders
 import numpy as np
-import glm
 import shaders
 import builder
 import vertice
-import posmath
-import time
+import area
 
 glfw.init()
 glfw.window_hint(glfw.VISIBLE, glfw.FALSE);
@@ -16,28 +13,24 @@ glfw.window_hint(glfw.VISIBLE, glfw.FALSE);
 window = builder.create_window(600,600)
 glfw.make_context_current(window)
 
-
 # Request a program and shader slots from GPU
 program  = glCreateProgram()
 shaders.compile_shaders(program)
 builder.build_program(program)
 
-
 # preparando espaço para 4 vértices usando 2 coordenadas (x,y)
-vertices = vertice.create_vertice_array(3)
+vertices = vertice.create_vertice_array(4)
 vertices[shaders.SN_POSITION_NAME] = [
-                            (+0.05, +0.00),
-                            (-0.05, +0.05),
-                            # (+0.00, +0.00),
-                            (-0.05, -0.05)
+                            (-0.20, -0.20),
+                            (+0.00, +0.20),
+                            (+0.00, -0.10),
+                            (+0.20, -0.20),
                         ]
-
 
 # Upload data
 buffer_VBO = builder.request_buffer_slot()
 glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL_DYNAMIC_DRAW) ## lembre que é global
 glBindBuffer(GL_ARRAY_BUFFER, buffer_VBO) ## vincula a gpu
-
 
 # Bind the position attribute
 # --------------------------------------
@@ -46,11 +39,13 @@ offset = ctypes.c_void_p(0)
 
 
 loc = glGetAttribLocation(program, shaders.SN_POSITION_NAME)
+
 glEnableVertexAttribArray(loc)
 
 glVertexAttribPointer(loc, 2, GL_FLOAT, False, stride, offset)
 
 loc_color = glGetUniformLocation(program, shaders.SN_COLOR)
+
 R = 0.7
 G = 0.0
 B = 0.2
@@ -65,18 +60,9 @@ def key_event(window,key,scancode,action,mods,scale=-1):
     if key == 83: t_y -= 0.01*scale #baixo
     if key == 65: t_x -= 0.01*scale #esquerda
 
-
-    
 glfw.set_key_callback(window,key_event)
 
 glfw.show_window(window)
-
-import math
-
-angulo = 0
-t_x = 0
-t_y = 0
-
 
 def multiplica_matriz(a,b):
     m_a = a.reshape(4,4)
@@ -88,51 +74,27 @@ def multiplica_matriz(a,b):
 glEnable(GL_BLEND);
 glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
+area_obj = area.Area(vertices)
 
-def pipeline(angle, t_x, t_y):
-    """
-    Retorna a matriz de transformação combinada.
-    
-    Parâmetros:
-        angle: Ângulo de rotação (em radianos).
-        t_x: Translação no eixo X.
-        t_y: Translação no eixo Y.
-    
-    Retorno:
-        Matriz de transformação 4x4.
-    """
-    # 1. Translação para a origem
-    pos = posmath.get_transl_matrix(-t_x, -t_y)
-    
-    # 2. Rotação em torno da origem
-    rotation = posmath.get_rot_matrix(angle)
-    
-    # 3. Translação de volta para a posição original
-    # pos_anti = posmath.get_transl_matrix(t_x, t_y)
-    
-    # Combina as matrizes: pos_anti @ rotation @ pos
-    # return pos_anti @ rotation @ pos
-    return pos @ rotation
 while not glfw.window_should_close(window):
     
     glClear(GL_COLOR_BUFFER_BIT) 
     glClearColor(1.0, 1.0, 1.0, 1.0)
-    
-    angulo -= 0.01
-    matriz_transformacao = pipeline(angulo,t_x,t_y) ## matriz que aplica a transformação nos vértices    
-    
-    # pede a localizacao da variavel mat_transformation no shader
+
+    area_obj.modify(0.0, 0.0, 0.001)
+
+    # Chama o método draw da instância e obtém a matriz de transformação
+    matriz_transformacao = area_obj.draw()
+
     loc = glGetUniformLocation(program, shaders.SN_MAT_TRANSFORMATION)
-    
-    # set mat_transformation com a mat_rotation
     glUniformMatrix4fv(loc, 1, GL_TRUE, matriz_transformacao)
+
+    glPolygonMode(GL_FRONT_AND_BACK,GL_LINE) ## ative esse comando para enxergar os triângulos
     
-    #glPolygonMode(GL_FRONT_AND_BACK,GL_LINE) ## ative esse comando para enxergar os triângulos
-    
-    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4) ## escreve os pontos
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4) ## desenha os pontos
     glUniform4f(loc_color, R, G, B, 1.0) ### modificando a cor do objeto!
-    
+
     glfw.swap_buffers(window)
-    glfw.poll_events() 
-    # time.sleep(0.1)
+    glfw.poll_events()
+
 glfw.terminate()
